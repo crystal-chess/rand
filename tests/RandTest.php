bf8dc6b65fb0b42ac692970ea14c60100d068234
ezoheux <46251064+ezoheux@users.noreply.github.com>
2019-01-01 22:34:21 -0500

    Update RandTest.php

--- tests/RandTest.php
+++ tests/RandTest.php
@@ -1,1 +1,30 @@
+<?php
 
+declare(strict_types=1);
+/**
+ * Crystal Chess - Generate random values..
+ *
+ * @author Crystal Chess Contributors <https://github.com/orgs/crystal-chess/people>
+ *
+ * @link <https://github.com/crystal-chess/hasher> Crystal Hasher.
+ */
+
+namespace CrystalChess\Chess;
+
+use CrystalChess\Rand;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Rand test case.
+ */
+class RandTest extends TestCase
+{
+    public function testRandomCases()
+    {
+        $rand = new Rand();
+        $bool = $rand->getBool();
+        $this->assertTrue(is_bool($bool));
+        $str = $rand->getString();
+        $this->assertTrue(is_string($str));
+    }
+}
