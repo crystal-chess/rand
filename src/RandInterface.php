21b0e7c0fea8426e4edc1e951a3dd00b3a4161d6
ezoheux <46251064+ezoheux@users.noreply.github.com>
2019-01-01 22:27:19 -0500

    Update RandInterface.php

--- src/RandInterface.php
+++ src/RandInterface.php
@@ -1,1 +1,36 @@
+<?php
+declare(strict_types=1);
+/**
+ * Crystal Chess - Generate random values.
+ *
+ * @author Crystal Chess Contributors <https://github.com/orgs/crystal-chess/people>
+ *
+ * @link <https://github.com/crystal-chess/hasher> Crystal Hasher.
+ */
+namespace CrystalChess;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use function random_int;
+use function mb_strlen;
 
+/**
+ * The rand interface.
+ */
+interface RandInterface
+{
+    /**
+     * Generate a random bool value.
+     *
+     * @return bool Returns a random bool value.
+     */
+    public function getBool(): bool;
+
+    /**
+     * Generate a random string value.
+     *
+     * @param int    $length   The string length.
+     * @param string $keyspace The list of characters allowed to use.
+     *
+     * @return bool Returns true if the hash needs a rehash and false if not.
+     */
+    public function getString(int $length = 16, string $keyspace = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'): string;
+}
